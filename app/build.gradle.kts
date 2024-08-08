9e5d512e3a0ea8196288c42f24858b80b8dbb264
Denis Makarov <d.makarov44@yandex.ru>
2024-08-09 02:26:16 +0300

    второй шаг, полет нормальный

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id ("application")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
